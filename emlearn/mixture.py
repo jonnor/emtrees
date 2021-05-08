0409eba6fc1dbfab2cfe23d8f20f3d1d6499250a
Jon Nordby <jononor@gmail.com>
2021-05-08 11:56:41 +0200

    TEMP: start reversing

--- emlearn/mixture.py
+++ emlearn/mixture.py
@@ -49,6 +49,74 @@ https://github.com/vlfeat/vlfeat/blob/master/vl/gmm.c#L712
 
 """
 
+from sklearn.mixture._gaussian_mixture import _compute_log_det_cholesky
+from sklearn.utils.extmath import row_norms
+np = numpy
+
+
+def _estimate_log_gaussian_prob(X, means, precisions_chol, covariance_type):
+    """Estimate the log Gaussian probability.
+    Parameters
+    ----------
+    X : array-like of shape (n_samples, n_features)
+    means : array-like of shape (n_components, n_features)
+    precisions_chol : array-like
+        Cholesky decompositions of the precision matrices.
+        'full' : shape of (n_components, n_features, n_features)
+        'tied' : shape of (n_features, n_features)
+        'diag' : shape of (n_components, n_features)
+        'spherical' : shape of (n_components,)
+    covariance_type : {'full', 'tied', 'diag', 'spherical'}
+    Returns
+    -------
+    log_prob : array, shape (n_samples, n_components)
+    """
+    n_samples, n_features = X.shape
+    n_components, _ = means.shape
+    # det(precision_chol) is half of det(precision)
+    log_det = _compute_log_det_cholesky(
+        precisions_chol, covariance_type, n_features)
+
+    if covariance_type == 'full':
+        log_prob = np.empty((n_samples, n_components))
+        for k, (mu, prec_chol) in enumerate(zip(means, precisions_chol)):
+
+            if True:
+                #for x in X:
+                print("ss", X.shape, mu.shape, prec_chol.shape)
+                y = np.matmul(X, prec_chol) - np.matmul(mu, prec_chol)
+                print("yy", y.shape, n_samples, n_features, n_components)
+                p = np.sum(np.square(y), axis=1) # sum over features
+                print("p", p.shape, p)
+                log_prob[:, k] = p          
+
+            else:
+                print("ss", X.shape, mu.shape, prec_chol.shape)
+                y = np.dot(X, prec_chol) - np.dot(mu, prec_chol)
+                print("yy", y.shape, n_samples, n_features, n_components)
+                p = np.sum(np.square(y), axis=1) # sum over features
+                print("p", p.shape, p)
+                log_prob[:, k] = p
+
+    elif covariance_type == 'tied':
+        log_prob = np.empty((n_samples, n_components))
+        for k, mu in enumerate(means):
+            y = np.dot(X, precisions_chol) - np.dot(mu, precisions_chol)
+            log_prob[:, k] = np.sum(np.square(y), axis=1)
+
+    elif covariance_type == 'diag':
+        precisions = precisions_chol ** 2
+        log_prob = (np.sum((means ** 2 * precisions), 1) -
+                    2. * np.dot(X, (means * precisions).T) +
+                    np.dot(X ** 2, precisions.T))
+
+    elif covariance_type == 'spherical':
+        precisions = precisions_chol ** 2
+        log_prob = (np.sum(means ** 2, 1) * precisions -
+                    2 * np.dot(X, means.T * precisions) +
+                    np.outer(row_norms(X, squared=True), precisions))
+    return -.5 * (n_features * np.log(2 * np.pi) + log_prob) + log_det
+
 class Wrapper:
     def __init__(self, estimator, classifier, dtype='float'):
         self.dtype = dtype
@@ -75,7 +143,7 @@ class Wrapper:
         self._weights = estimator.weights_
 
     def predict_proba(self, X):
-        from sklearn.mixture._gaussian_mixture import _estimate_log_gaussian_prob
+        #from sklearn.mixture._gaussian_mixture import _estimate_log_gaussian_prob
         predictions = _estimate_log_gaussian_prob(X, self._means,
                                     self._precisions_col, self._covariance_type)
 
